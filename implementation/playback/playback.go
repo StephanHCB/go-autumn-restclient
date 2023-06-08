9ca241a4b3c1c0645d98c074c068c1cb678dd99e
StephanHCB <sbs_github_u43a@packetloss.de>
2023-06-08 13:42:30 +0200

    feat(#19): allow external filename functions and give access to request body

--- implementation/playback/playback.go
+++ implementation/playback/playback.go
@@ -14,84 +14,98 @@ import (
 const PlaybackRewritePathEnvVariable = "GO_AUTUMN_RESTCLIENT_PLAYBACK_REWRITE_PATH"
 
 type PlaybackImpl struct {
-	RecorderPath        string
-	RecorderRewritePath string
-	// Now is exposed so tests can fixate the time by overwriting this field
-	Now func() time.Time
+	RecorderPath                string
+	RecorderRewritePath         string
+	ConstructFilenameCandidates []aurestrecorder.ConstructFilenameFunction
+	Now                         func() time.Time
+}
+
+type PlaybackOptions struct {
+	// ConstructFilenameCandidates contains filename constructor functions.
+	//
+	// The first one is considered "canonical", for all others, a log entry is printed that instructs the user
+	// to rename the file.
+	ConstructFilenameCandidates []aurestrecorder.ConstructFilenameFunction
+	NowFunc                     func() time.Time
 }
 
 // New builds a new http client simulator based on playback.
 //
 // Use this in your tests.
-func New(recorderPath string) aurestclientapi.Client {
+//
+// You can optionally add a PlaybackOptions instance to your call. The ... is really just so it's an optional argument.
+func New(recorderPath string, additionalOptions ...PlaybackOptions) aurestclientapi.Client {
+	filenameCandidates := []aurestrecorder.ConstructFilenameFunction{
+		aurestrecorder.ConstructFilenameV3WithBody,
+		aurestrecorder.ConstructFilenameWithBody,
+		aurestrecorder.ConstructFilenameV2WithBody,
+	}
+	nowFunc := time.Now
+	for _, o := range additionalOptions {
+		if len(o.ConstructFilenameCandidates) > 0 {
+			filenameCandidates = o.ConstructFilenameCandidates
+		}
+		if o.NowFunc != nil {
+			nowFunc = o.NowFunc
+		}
+	}
 	return &PlaybackImpl{
-		RecorderPath:        recorderPath,
-		RecorderRewritePath: os.Getenv(PlaybackRewritePathEnvVariable),
-		Now:                 time.Now,
+		RecorderPath:                recorderPath,
+		RecorderRewritePath:         os.Getenv(PlaybackRewritePathEnvVariable),
+		ConstructFilenameCandidates: filenameCandidates,
+		Now:                         nowFunc,
 	}
 }
 
-func (c *PlaybackImpl) Perform(ctx context.Context, method string, requestUrl string, _ interface{}, response *aurestclientapi.ParsedResponse) error {
-	filename, err := aurestrecorder.ConstructFilenameV3(method, requestUrl)
-	if err != nil {
-		return err
-	}
+func (c *PlaybackImpl) Perform(ctx context.Context, method string, requestUrl string, requestBody interface{}, response *aurestclientapi.ParsedResponse) error {
+	canonicalFilename := ""
+	var originalError error
+	for i, constructFilenameCandidate := range c.ConstructFilenameCandidates {
+		filename, err := constructFilenameCandidate(method, requestUrl, requestBody)
+		if err != nil {
+			return err
+		}
+		if i == 0 {
+			canonicalFilename = filename
+		}
 
-	jsonBytes, err := os.ReadFile(filepath.Join(c.RecorderPath, filename))
-	if err != nil {
-		// try old filename for compatibility (cannot fail if ConstructFilenameV2 didn't)
-		filenameOldV1, _ := aurestrecorder.ConstructFilename(method, requestUrl)
-
-		jsonBytesOldV1, errWithOldFilenameV1 := os.ReadFile(filepath.Join(c.RecorderPath, filenameOldV1))
-		if errWithOldFilenameV1 != nil {
-			// try old filename for compatibility (cannot fail if ConstructFilenameV2 didn't)
-			filenameOldV2, _ := aurestrecorder.ConstructFilenameV2(method, requestUrl)
-
-			jsonBytesOldV2, errWithOldFilenameV2 := os.ReadFile(filepath.Join(c.RecorderPath, filenameOldV2))
-			if errWithOldFilenameV2 != nil {
-				// but return original error if that also fails
-				return err
-			} else {
-				aulogging.Logger.Ctx(ctx).Info().Printf("use of deprecated recorder filename (v2) '%s', please move to '%s'", filenameOldV2, filename)
-
-				_ = c.rewriteFileIfConfigured(ctx, filenameOldV2, filename)
-
-				filename = filenameOldV2
-				jsonBytes = jsonBytesOldV2
+		jsonBytes, err := os.ReadFile(filepath.Join(c.RecorderPath, filename))
+		if err != nil {
+			if i == 0 {
+				originalError = err
 			}
 		} else {
-			aulogging.Logger.Ctx(ctx).Info().Printf("use of deprecated recorder filename (v1) '%s', please move to '%s'", filenameOldV1, filename)
+			// successfully read
+			if i > 0 {
+				aulogging.Logger.Ctx(ctx).Info().Printf("use of deprecated recorder filename '%s', please move to '%s'", filename, canonicalFilename)
+			}
 
-			_ = c.rewriteFileIfConfigured(ctx, filenameOldV1, filename)
+			_ = c.rewriteFileIfConfigured(ctx, filename, canonicalFilename)
 
-			filename = filenameOldV1
-			jsonBytes = jsonBytesOldV1
+			recording := aurestrecorder.RecorderData{}
+			err = json.Unmarshal(jsonBytes, &recording)
+			if err != nil {
+				return err
+			}
+
+			response.Header = recording.ParsedResponse.Header
+			response.Status = recording.ParsedResponse.Status
+			response.Time = c.Now()
+
+			// cannot just assign the body, need to re-parse into the existing pointer - using a json round trip
+			bodyJsonBytes, err := json.Marshal(recording.ParsedResponse.Body)
+			if err != nil {
+				return err
+			}
+			err = json.Unmarshal(bodyJsonBytes, response.Body)
+			if err != nil {
+				return err
+			}
+
+			return recording.Error
 		}
-	} else {
-		_ = c.rewriteFileIfConfigured(ctx, filename, filename)
 	}
-
-	recording := aurestrecorder.RecorderData{}
-	err = json.Unmarshal(jsonBytes, &recording)
-	if err != nil {
-		return err
-	}
-
-	response.Header = recording.ParsedResponse.Header
-	response.Status = recording.ParsedResponse.Status
-	response.Time = c.Now()
-
-	// cannot just assign the body, need to re-parse into the existing pointer - using a json round trip
-	bodyJsonBytes, err := json.Marshal(recording.ParsedResponse.Body)
-	if err != nil {
-		return err
-	}
-	err = json.Unmarshal(bodyJsonBytes, response.Body)
-	if err != nil {
-		return err
-	}
-
-	return recording.Error
+	return originalError
 }
 
 func (c *PlaybackImpl) rewriteFileIfConfigured(ctx context.Context, fileNameFrom string, fileNameTo string) error {
--- implementation/recorder/recorder_test.go
+++ implementation/recorder/recorder_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestConstructFilenameLong(t *testing.T) {
 	requestUrl := "https://some.super.long.server.name.that.hopefully.does.not.exist/api/rest/v1/v2/v3/v4/this/is/intentionally/very/very/very/very/long/djkfjhdalsfhdsahjflkdjsahfkjlsdhafjkdshafkjlsdahf/asdfjkldsahfkjlfad/dskjfhjkdsfahlk/sdafjkhsdafklhreuih/dfgjgkjlhgjlkhg?asjdfhlkhewuirfhkjsdhlk=kjhrelrukihsjd&fsdkjhfdjklhsdf=werjkyewuiryuweiry&sfuyfddsjkhjkldsfhldkfs=sdjhdflksjhfdhsf"
-	actual, err := ConstructFilename("GET", requestUrl)
+	actual, err := ConstructFilenameWithBody("GET", requestUrl, nil)
 	expected := "request_get_%2Fapi%2Frest%2Fv1%2Fv2%2Fv3%2Fv4%2Fthis%2Fis%2Fintentionally%2Fvery%2Fvery%2Fvery%2Fvery%2Flong%2Fdjkfjhdalsfhdsahjflkd_fb2e3656d88910ffc49023f99f5e0df6.json"
 	require.Nil(t, err)
 	require.Equal(t, expected, actual)
@@ -15,7 +15,7 @@ func TestConstructFilenameLong(t *testing.T) {
 
 func TestConstructFilenameShort(t *testing.T) {
 	requestUrl := "https://some.short.server.name/api/rest/v1/kittens"
-	actual, err := ConstructFilename("GET", requestUrl)
+	actual, err := ConstructFilenameWithBody("GET", requestUrl, nil)
 	expected := "request_get_%2Fapi%2Frest%2Fv1%2Fkittens_d41d8cd98f00b204e9800998ecf8427e.json"
 	require.Nil(t, err)
 	require.Equal(t, expected, actual)
@@ -23,7 +23,7 @@ func TestConstructFilenameShort(t *testing.T) {
 
 func TestConstructFilenameV2Long(t *testing.T) {
 	requestUrl := "https://some.super.long.server.name.that.hopefully.does.not.exist/api/rest/v1/v2/v3/v4/this/is/intentionally/very/very/very/very/long/djkfjhdalsfhdsahjflkdjsahfkjlsdhafjkdshafkjlsdahf/asdfjkldsahfkjlfad/dskjfhjkdsfahlk/sdafjkhsdafklhreuih/dfgjgkjlhgjlkhg?asjdfhlkhewuirfhkjsdhlk=kjhrelrukihsjd&fsdkjhfdjklhsdf=werjkyewuiryuweiry&sfuyfddsjkhjkldsfhldkfs=sdjhdflksjhfdhsf"
-	actual, err := ConstructFilenameV2("GET", requestUrl)
+	actual, err := ConstructFilenameV2WithBody("GET", requestUrl, nil)
 	expected := "request_get_api-rest-v1-v2-v3-v4-this-is-intentionally-very-very-very-very-long-djkfjhdalsfhdsahjflkd_fb2e3656.json"
 	require.Nil(t, err)
 	require.Equal(t, expected, actual)
@@ -31,7 +31,7 @@ func TestConstructFilenameV2Long(t *testing.T) {
 
 func TestConstructFilenameV2Short(t *testing.T) {
 	requestUrl := "https://some.short.server.name/api/rest/v1/kittens"
-	actual, err := ConstructFilenameV2("GET", requestUrl)
+	actual, err := ConstructFilenameV2WithBody("GET", requestUrl, nil)
 	expected := "request_get_api-rest-v1-kittens_d41d8cd9.json"
 	require.Nil(t, err)
 	require.Equal(t, expected, actual)
@@ -39,7 +39,7 @@ func TestConstructFilenameV2Short(t *testing.T) {
 
 func TestConstructFilenameV3Long(t *testing.T) {
 	requestUrl := "https://some.super.long.server.name.that.hopefully.does.not.exist/api/rest/v1/v2/v3/v4/this/is/intentionally/very/very/very/very/long/djkfjhdalsfhdsahjflkdjsahfkjlsdhafjkdshafkjlsdahf/asdfjkldsahfkjlfad/dskjfhjkdsfahlk/sdafjkhsdafklhreuih/dfgjgkjlhgjlkhg?asjdfhlkhewuirfhkjsdhlk=kjhrelrukihsjd&fsdkjhfdjklhsdf=werjkyewuiryuweiry&sfuyfddsjkhjkldsfhldkfs=sdjhdflksjhfdhsf"
-	actual, err := ConstructFilenameV3("GET", requestUrl)
+	actual, err := ConstructFilenameV3WithBody("GET", requestUrl, nil)
 	expected := "request_get_api-rest-v1-v2-v3-v4-this-is-intentionally-very-very-very-very-long-djkfjhdalsfhdsahjflkd_fb2e3656.json"
 	require.Nil(t, err)
 	require.Equal(t, expected, actual)
@@ -47,7 +47,7 @@ func TestConstructFilenameV3Long(t *testing.T) {
 
 func TestConstructFilenameV3Short(t *testing.T) {
 	requestUrl := "https://some.short.server.name/api/rest/v1/kittens"
-	actual, err := ConstructFilenameV3("GET", requestUrl)
+	actual, err := ConstructFilenameV3WithBody("GET", requestUrl, nil)
 	expected := "request_get_api-rest-v1-kittens_d41d8cd9.json"
 	require.Nil(t, err)
 	require.Equal(t, expected, actual)
@@ -55,10 +55,10 @@ func TestConstructFilenameV3Short(t *testing.T) {
 
 func TestConstructEqualFilenameV3ForDifferentQueryParameterOrder(t *testing.T) {
 	requestUrl1 := "https://some.short.server.name/api/rest/v1/kittens?a=123&z=o&v=666"
-	actual1, _ := ConstructFilenameV3("GET", requestUrl1)
+	actual1, _ := ConstructFilenameV3WithBody("GET", requestUrl1, nil)
 
 	requestUrl2 := "https://some.short.server.name/api/rest/v1/kittens?z=o&v=666&a=123"
-	actual2, _ := ConstructFilenameV3("GET", requestUrl2)
+	actual2, _ := ConstructFilenameV3WithBody("GET", requestUrl2, nil)
 
 	require.Equal(t, actual1, actual2)
 }
